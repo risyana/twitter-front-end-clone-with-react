8bc751401b1d76c40638ebb23fbfcd589c78eebb
Eka Risyana <risyana@gmail.com>
2018-06-13 07:49:16 +0700

    using eslint to follow airbnb style

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb",
+    "globals": {
+        "window": true,
+        "document": true,
+        "navigator": true,
+        "URL": true,
+        "fetch": true,
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "react/jsx-filename-extension": 0,
+        "no-use-before-define": 0,
+        "no-param-reassign": 0,
+    }
+};
